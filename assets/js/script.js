dd3d8c64750f90555a04a32ece68ebc67ec1c9c6
JosephJamesCoop <91605342+JosephJamesCoop@users.noreply.github.com>
2021-12-14 07:48:38 -0800

    pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//call api
+
+//create search box
+
+// put search box through function that will link to API
+
+//pull search results from box and create li/buttons to add to ul id=city-button
+
+//buttons will need to be stored to local storage and retrived as well.
+
+// generate results from search to display results in div today.
+
+//generate results from search to display results in div five-day
+
